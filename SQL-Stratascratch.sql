156694d3629fea00c96e18daf198056150bb6af7
Eric Wan <ericwan@andrew.cmu.edu>
2023-01-29 15:40:32 -0600

    1/29 SQL
    Another hard from stratascratch

--- SQL-Stratascratch.sql
+++ SQL-Stratascratch.sql
@@ -17,4 +17,29 @@ from
 where 
     state_rank <= 5
 
--- 
+-- https://platform.stratascratch.com/coding/9632-host-popularity-rental-prices?tabname=question
+with host_concat as (
+    select 
+        distinct concat(price, room_type, host_since, zipcode, number_of_reviews) as host_id,
+        number_of_reviews, 
+        price
+    from airbnb_host_searches
+),
+host_popularity as (
+    select case 
+        when number_of_reviews = 0 then 'NEW'
+        when number_of_reviews between 1 and 5 then 'Rising'
+        when number_of_reviews between 6 and 15 then 'Trending Up'
+        when number_of_reviews between 16 and 40 then 'Popular'
+        when number_of_reviews > 40 then 'Hot'
+    end as host_pop_rating,
+    min(price) as min_price,
+    avg(price) as avg_price,
+    max(price) as max_price
+    from host_concat
+    group by host_pop_rating
+)
+select
+    *
+from
+    host_popularity
