1c18c7917cf2536256421bbd48c94abe7633b1c8
Eric Wan <ericwan@andrew.cmu.edu>
2023-02-07 23:35:43 -0600

    2/7

--- python-LC.py
+++ python-LC.py
@@ -18,3 +18,28 @@ while i<n:
     i+=1
     
 return output
+
+# https://leetcode.com/problems/01-matrix/
+dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+m, n = len(matrix), len(matrix[0])
+queue = collections.deque()
+
+res = [[-1 for _ in range(n)] for _ in range(m)]
+
+for i in range(m):
+    for j in range(n):
+        if matrix[i][j] == 0:
+            # The distance to itself is 0 and add all sources here to queue
+            res[i][j] = 0
+            queue.append((i, j))
+            
+while queue:
+    curI, curJ = queue.popleft()
+    for i, j in dirs:
+        neighBorI, neighBorJ = curI + i, curJ + j
+        # Validate all the neighbors and validate the distance as well
+        if 0 <= neighBorI < m and 0 <= neighBorJ < n and res[neighBorI][neighBorJ] == -1:
+            res[neighBorI][neighBorJ] = res[curI][curJ] + 1
+            queue.append((neighBorI, neighBorJ))
+
+return res
