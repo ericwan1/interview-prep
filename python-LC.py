875c9fd95e14cd42e5da18fce50f1d4a7ac5c064
Eric Wan <ericwan@andrew.cmu.edu>
2023-02-15 23:32:56 -0600

    2/15

--- python-LC.py
+++ python-LC.py
@@ -160,3 +160,35 @@ def wordBreak(self, s, wordDict):
                     visited.add(new_substr)
 
     return False    
+
+
+# https://leetcode.com/problems/count-number-of-nice-subarrays/
+# Solution from user Harshhx
+# Two pointers makes for an intuitive take on calculating the correct subarrays
+def numberOfSubarrays(self, nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: int
+    """
+    right ,left = 0,0
+    ans = 0 
+    odd_cnt = 0
+    ans = 0
+    cur_sub_cnt = 0
+    
+    for right in range(len(nums)):
+        
+        if nums[right]%2 == 1:
+            odd_cnt += 1
+            cur_sub_cnt = 0
+            
+        while odd_cnt == k:
+            if nums[left]%2 == 1:
+                odd_cnt -= 1
+            cur_sub_cnt += 1
+            left += 1
+            
+        ans += cur_sub_cnt
+        
+    return ans  
